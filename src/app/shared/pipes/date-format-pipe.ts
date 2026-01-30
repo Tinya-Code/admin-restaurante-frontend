36688073a421bf54e8cb9272b9157d128c05731e
antony.sl <antony.g.s.lopez@gmail.com>
2026-01-30 01:10:40 -0500

    Pipe personalizado ( date-format-pipe ) agregado

--- src/app/shared/pipes/date-format-pipe.ts
+++ src/app/shared/pipes/date-format-pipe.ts
@@ -4,9 +4,15 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateFormat'
 })
 export class DateFormatPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  
+  // funcions para transformar la fecha a un formato específico
+  transform(date: Date|string|number|null, mode: "short"| "long"|"full" = "short" ): string {
+    // verificamos que la vecha sea valida
+    if(!date){
+      return 'Sin fecha valida';
+    }
+    // retornamos la fecha formateada seung el modo ingresado
+    return new Intl.DateTimeFormat('en-PE', {dateStyle: mode}).format(new Date(date))
   }
 
 }
