ea3c2bf3adaa29ec604081f927a0710853ef7303
antony.sl <antony.g.s.lopez@gmail.com>
2026-01-30 22:49:21 -0500

    Agregar validación de fechas inválidas en date-Format-Pipe

--- src/app/shared/pipes/date-format-pipe.ts
+++ src/app/shared/pipes/date-format-pipe.ts
@@ -6,10 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateFormatPipe implements PipeTransform {
   
   // funcions para transformar la fecha a un formato específico
-  transform(date: Date|string|number|null, mode: "short"| "long"|"full" = "short" ): string {
-    // verificamos que la vecha sea valida
-    if(!date){
-      return 'Sin fecha valida';
+  transform(  date: Date|string|number|null, 
+              mode: "short"| "long"|"full" = "short" )
+              :string 
+            {
+    // verificamos que la vecha sea valida y que sean un valor utilizable
+    if(date === null || date === undefined || isNaN(new Date(date).getTime()) ) {
+      return '';
     }
     // retornamos la fecha formateada seung el modo ingresado
     return new Intl.DateTimeFormat('es-PE', {dateStyle: mode}).format(new Date(date))
