9c6e7c48343eebe123219b2d3f720f378de6fc72
antony.sl <antony.g.s.lopez@gmail.com>
2026-02-01 01:34:21 -0500

    agrega @angular/fire para auth y authService : login google y logOut completos

---
+++ src/app/features/auth/services/authService.ts
@@ -0,0 +1,40 @@
+import { Injectable, inject } from '@angular/core';
+import { Auth } from '../../../core/services/auth';
+import { Notification } from '../../../core/services/notification';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class authService {
+
+    private auth: Auth = inject(Auth)
+    private notificaction: Notification = inject(Notification);
+    private router: Router = inject(Router);
+
+
+    async loginGoogle() : Promise < void > {
+      try{
+        const user = await this.auth.loginWithGoogle();    
+        // mostramos nitificacion de exito
+        this.notificaction.success(`bienvenido ${user?.displayName}`);
+        // redirigimos a la pagina principal
+        this.router.navigate([`/admin/home`]);
+      }
+      catch (error) {
+        this.notificaction.error(`Error al iniciar sesion con Google: ${error}`);
+    }};
+
+    async closeSession(): Promise<void> {
+      try {
+        await this.auth.logOut();
+      
+        // mostramos notifdicacion de exito
+        this.notificaction.info('Sesion cerrada correctamente');
+            // una vez cerrada la seccion redirigimos a login
+        this.router.navigate([`/login`])
+      }catch(error){
+        this.notificaction.error(`Error al cerrar sesion: ${error}`);
+      }
+    }
+}
