8a8ebc80eec593a5a4a0f51bc4a9450e70273028
WilO22 <wilmer.ogosi.22@gmail.com>
2026-02-01 14:12:10 -0500

    feat(TC-22): implementar pipe currency para formateo de soles peruanos

--- src/app/shared/pipes/currency-pipe.ts
+++ src/app/shared/pipes/currency-pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CurrencyPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string | number | null | undefined): string {
+      //convertimos el valor a número
+      const numeroConvertido = Number(value);
+
+      //validamos si la conversion fue exitosa
+      if(isNaN(numeroConvertido)){
+        return 'S/ 0.00';
+      }
+
+    //Formateamos el numero con 2  decimales y agregamos el simbolo
+    return `S/ ${numeroConvertido.toFixed(2)}`;
   }
 
 }
