dec23ba1c03288f3b985dd19757f28c40f7683c0
Pedro Alejandro <alejandroleonpedro7@gmail.com>
2026-02-10 03:43:55 -0500

    fix: style product-list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from "./shared/components/toast/toast";
-import { BackButton } from './shared/components/back-button/back-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast, BackButton],
+  imports: [RouterOutlet, Toast],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
