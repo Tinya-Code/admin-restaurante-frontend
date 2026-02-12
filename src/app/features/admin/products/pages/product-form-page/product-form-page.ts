54cfef6dec74b2fb58fba7008b134b51d7f8b752
Pedro Alejandro <alejandroleonpedro7@gmail.com>
2026-02-11 22:35:14 -0500

    formulario conectado

--- src/app/features/admin/products/pages/product-form-page/product-form-page.ts
+++ src/app/features/admin/products/pages/product-form-page/product-form-page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductForm } from '../../components/product-form/product-form';
-
+import { ProductCreate, ProductUpdate } from '../../../../../core/models/product.model';
+import { ProductService } from '../../services/product';
+import { Notification } from '../../../../../core/services/notification';
 @Component({
   selector: 'app-product-form-page',
   imports: [ProductForm],
@@ -8,5 +10,36 @@ import { ProductForm } from '../../components/product-form/product-form';
   styleUrl: './product-form-page.css',
 })
 export class ProductFormPage {
-
+  private readonly productService = inject(ProductService);
+  private readonly notification = inject(Notification);
+  onFormSubmit(event: { data: ProductCreate | ProductUpdate; image?: File }): void {
+    if ('id' in event.data) {
+      this.productService
+        .updateProduct(event.data.id, event.data as ProductUpdate, event.image)
+        .subscribe({
+          next: (res) => {
+            this.notification.success('Producto actualizado correctamente');
+            console.log('Producto actualizado', res);
+          },
+          error: (err) => {
+            this.notification.error('Error al actualizar producto');
+            console.error(err);
+          },
+        });
+    } else {
+      this.productService.createProduct(event.data as ProductCreate, event.image).subscribe({
+        next: (res) => {
+          this.notification.success('Producto creado correctamente');
+          console.log('Producto creado', res);
+        },
+        error: (err) => {
+          this.notification.error('Error al crear producto');
+          console.error(err);
+        },
+      });
+    }
+  }
+  onFormCancel(): void {
+    this.notification.info('Formulario cancelado');
+  }
 }
--- src/app/shared/components/search-bar/search-bar.ts
+++ src/app/shared/components/search-bar/search-bar.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { LucideAngularModule, Search } from 'lucide-angular';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 @Component({
@@ -8,27 +8,31 @@ import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
   styleUrl: './search-bar.css',
 })
 export class SearchBar {
-
   // Icon
-  searchIcon = Search
+  searchIcon = Search;
 
   // iniciamos el form builder
   private fb = inject(FormBuilder);
+  searchChange = output<string>();
 
   // signal para almacenar la palabra buscada
   word = this.fb.group({
-    searchWord: [ `` , [Validators.minLength(4), Validators.maxLength(100)]]
-  })
+    searchWord: [``, [Validators.minLength(4), Validators.maxLength(100)]],
+  });
 
+  clear(): void {
+    this.word.reset();
+    this.searchChange.emit('');
+  }
   // adjudicamos al metodo onsearch la logica de busqueda con retorno de la palabra buscada para conprovar su funcionamiento
-  onsearch(): string {
+  onsearch(): void {
     if (this.word.invalid) {
       console.log('El termino de busqueda debe tener al menos 4 caracteres');
-      return '';
+      return;
     }
     const { searchWord } = this.word.value;
-    console.log(searchWord);
-
-    return searchWord as string;
+    console.log(`Palabra buscada:`, searchWord);
+    this.searchChange.emit(searchWord as string);
+    console.log('Emitiendo palabra buscada al componente padre');
   }
 }
